e963eaabca4b6f6fc869aeb1419ee0136cf3346a
Anjlovrgse <anjalovarghese4@gmail.com>
2025-04-29 19:06:55 +0530

    Updated

--- TwoSum.py
+++ TwoSum.py
@@ -9,3 +9,12 @@ class Solution(object):
                 return [prev_map[diff], i]
             prev_map[num] = i
         return []  # Return an empty list if no solution is found
+    
+
+    
+
+#Iteration steps,if target is 9:
+
+#i=0, num=2 → diff = 7 → 7 not in prev_map → store 2:0
+
+#i=1, num=7 → diff = 2 → 2 is in prev_map → return [0, 1]
